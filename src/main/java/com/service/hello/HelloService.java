e74b36c5d32cd02c2bf24862b5a1d96d88b8510d
astroboy <toasadi@gmail.com>
2019-06-04 17:34:41 +0200

    Initial

---
+++ src/main/java/com/service/hello/HelloService.java
@@ -0,0 +1,18 @@
+package com.service.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloService {
+
+    @GetMapping()
+    public String getHello(){
+        return "Hello World";
+    }
+
+
+
+}
